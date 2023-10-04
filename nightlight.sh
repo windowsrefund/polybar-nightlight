878065abd8f30ea662df38d958a53a8b6f30b462
windowsrefund <mtf8>
2023-10-04 00:05:47 -0400

    detect displays

--- nightlight.sh
+++ nightlight.sh
@@ -1,41 +1,69 @@
 #!/usr/bin/env bash
 
-displays=(eDP-1)
+usage() {
+	echo "$(basename "$0") [+|-]"
+}
+
+mapfile -t displays < <(xrandr | grep -w connected | awk '{print $1}')
+
 profiles=(
-	1:1:1
-	1:.97:.94
-	1:.93:.88
-	1:.90:.81
-	1:.86:.73
-	1:.82:.64
-	1:.77:.54
-	1:.71:.42
-	1:.68:.35
-	1:.64:.28
-	1:.59:.18
-	1:.54:.08
-	1:.32:.03
 	1:.18:.01
+	1:.32:.03
+	1:.54:.08
+	1:.59:.18
+	1:.64:.28
+	1:.68:.35
+	1:.71:.42
+	1:.77:.54
+	1:.82:.64
+	1:.86:.73
+	1:.90:.81
+	1:.93:.88
+	1:.97:.94
+	1:1:1
 )
 
-config="${XDG_DATA_HOME:-$HOME/.local/share}/$(basename "$0" .sh)"
+[[ $# -gt 1 ]] && {
+	usage
+	exit 1
+}
 
-# shellcheck source=/dev/null
-source "$config"
+# default
+profile="${profiles[-1]}"
+config="$XDG_DATA_HOME/$(basename "$0" .sh)"
+index=-1
 
-len=${#profiles[@]}
-
-# shellcheck disable=SC2154
-case "$1" in
-	"+" )    new_ind=$((ind+1 > len-1 ? len-1 : ind+1));;
-	"-" )    new_ind=$((ind-1 < 0 ? 0 : ind-1));;
-esac
-
-new_prof="${profiles[$new_ind]}"
-
-for display in "${displays[@]}"; do
-	xrandr --output "$display" --gamma "${new_prof}"
+[[ -f "$config" ]] && source "$config"
+for i in "${!profiles[@]}"; do
+	if [[ "${profiles[$i]}" = "$profile" ]]; then
+		index="$i"
+		break
+	fi
 done
 
-echo "ind=${new_ind}" > "$config"
-echo "${new_prof}"
+profiles_max="$((${#profiles[@]} - 1))"
+current_choice="${profiles[$index]}"
+
+case "$1" in
+"+")
+	if [ "$index" -lt "$profiles_max" ]; then
+		new_choice="${profiles[$((index + 1))]}"
+	fi
+	;;
+
+"-")
+	if [ "$index" -gt 0 ]; then
+		new_choice="${profiles[$((index - 1))]}"
+	fi
+	;;
+esac
+
+if [ -n "$new_choice" ]; then
+	for display in "${displays[@]}"; do
+		xrandr --output "$display" --gamma "${new_choice}"
+	done
+
+	echo "profile=${new_choice}" >"$config"
+fi
+
+echo " ${new_choice:-$current_choice}"
